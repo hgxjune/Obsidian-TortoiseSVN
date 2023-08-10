4cd1336462686a163dc9f8d925ccab29902e4e04
hgxjune <hgxjune@qq.com>
2023-08-10 18:31:01 +0800

    add windows terminal setting.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
-import { App, Plugin, PluginSettingTab, Setting, FileSystemAdapter } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, FileSystemAdapter, TFile } from 'obsidian';
 import { exec } from "child_process";
+
+// Obsidian Plugin Developer Docs
+// https://luhaifeng666.github.io/obsidian-plugin-docs-zh/zh2.0/
 
 // Remember to rename these classes and interfaces!
 
 interface ObsidianTortoiseSVNSettings {
-	mySetting: string;
+	wt: boolean;
 }
 
 const DEFAULT_SETTINGS: ObsidianTortoiseSVNSettings = {
-	mySetting: 'default'
+	wt: false
 }
 
 export default class ObsidianTortoiseSVN extends Plugin {
@@ -24,12 +27,18 @@ export default class ObsidianTortoiseSVN extends Plugin {
 		this.registerEvent(
 			this.app.workspace.on("file-menu", (menu, file) => {
 				const adapter = this.app.vault.adapter as FileSystemAdapter;
+				const folder = (file as TFile).stat? file.parent?.path : file.path;
+				const path = adapter.getFullPath(folder? folder : file.path);
+				
+				// icon
+				// https://lucide.dev/
+
 				menu.addItem((item) => {
 					item
 						.setTitle("SVN 更新")
 						.setIcon("download-cloud")
 						.onClick(async () => {
-							exec("TortoiseProc /command:update /closeonend:0  /path:" + adapter.getFullPath(file.path));
+							exec("TortoiseProc /command:update /closeonend:0  /path:" + path);
 						});
 				});
 				menu.addItem((item) => {
@@ -37,9 +46,18 @@ export default class ObsidianTortoiseSVN extends Plugin {
 						.setTitle("SVN 提交")
 						.setIcon("upload-cloud")
 						.onClick(async () => {
-							exec("TortoiseProc /command:commit /closeonend:0  /path:" + adapter.getFullPath(file.path));
+							exec("TortoiseProc /command:commit /closeonend:0  /path:" + path);
 						});
 				});
+				if (this.settings.wt)
+					menu.addItem((item) => {
+						item
+							.setTitle("Windows Terminal")
+							.setIcon("terminal")
+							.onClick(async () => {
+								exec("wt -d " + path);
+							});
+					});
 			})
 		);
 
@@ -68,21 +86,20 @@ class SampleSettingTab extends PluginSettingTab {
 
 	display(): void {
 		const {containerEl} = this;
-
 		containerEl.empty();
-
 		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName('Windows Terminal')
+			.setDesc('Use Windows Terminal here')
+			.addToggle((toggle) =>
+			toggle
+				.setValue(this.plugin.settings.wt)
+				.onChange((value) => {
+					this.plugin.settings.wt = value;
+					this.display();
+					this.plugin.saveSettings();
+				})
+		);
 	}
 }
